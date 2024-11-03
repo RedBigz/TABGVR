94f049bc73b6080bbb4e435c18c3c008b4e74ab9
RedBigz <signoff@redbigz.com>
2024-11-03 16:20:45 +1000

    First Commit

---
+++ TABGVR/Plugin.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+
+namespace TABGVR;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Logger.LogInfo("TABGVR plugin loaded.");
+    }
+}
