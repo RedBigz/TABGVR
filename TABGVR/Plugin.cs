c9d2d52820f22c0e4ce9385de5d01abb82be7de1
RedBigz <signoff@redbigz.com>
2024-11-03 22:21:23 +1000

    Set logger over assembly

--- TABGVR/Plugin.cs
+++ TABGVR/Plugin.cs
@@ -1,12 +1,17 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 
 namespace TABGVR;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
+    internal new static ManualLogSource Logger;
+
     private void Awake()
     {
+        Logger = base.Logger;
+
         Logger.LogInfo("TABGVR plugin loaded.");
     }
 }
