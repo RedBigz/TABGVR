8b8b82810bd3f67f5d5303e06c2526cdfcc39bef
RedBigz <signoff@redbigz.com>
2024-11-05 20:02:41 +1000

    Add PlayerManager

---
+++ TABGVR/Player/PlayerManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace TABGVR.Player;
+
+public class PlayerManager
+{
+    public GameObject playerRoot;
+    public global::Player player;
+
+    public GameObject cameraObject;
+    public Camera cameraComponent;
+
+    public bool playerIsClient;
+
+    public PlayerManager(GameObject root)
+    {
+        playerRoot = root;
+        
+        player = root.GetComponent<global::Player>();
+        playerIsClient = player == global::Player.localPlayer; // i think this works
+        
+        cameraObject = playerRoot.transform.FindChildRecursive("Main Camera").gameObject;
+        cameraComponent = cameraObject.GetComponent<Camera>();
+    }
+
+    public static PlayerManager FromCamera(Camera camera) => new(camera.transform
+        .parent // Pivot
+        .parent // PositionShake
+        .parent // RotationShake
+        .parent // CameraPhysics
+        .parent // CameraRotationY
+        .parent // CameraRotationX
+        .parent // CameraMovement
+        .parent // Player
+        .gameObject);
+}
