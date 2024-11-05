159996a7571c33fb4303276867ddb0fd75663fb3
RedBigz <signoff@redbigz.com>
2024-11-05 20:01:11 +1000

    Fix touch controllers not working

--- TABGVR/XRLoader.cs
+++ TABGVR/XRLoader.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Management;
 using UnityEngine.XR.OpenXR;
+using UnityEngine.XR.OpenXR.Features;
+using UnityEngine.XR.OpenXR.Features.Interactions;
 
 namespace TABGVR;
 
@@ -15,23 +17,32 @@ public static class XRLoader
         var managerSettings = ScriptableObject.CreateInstance<XRManagerSettings>();
 
         var xrLoader = ScriptableObject.CreateInstance<OpenXRLoader>();
-        
+
         OpenXRSettings.Instance.renderMode = OpenXRSettings.RenderMode.MultiPass;
-        
+
         generalSettings.Manager = managerSettings;
-        
+
         managerSettings.loaders.Clear();
         managerSettings.loaders.Add(xrLoader);
 
+        var oculusTouch = ScriptableObject.CreateInstance<OculusTouchControllerProfile>();
+
+        oculusTouch.enabled = true;
+
+        OpenXRSettings.Instance.features = new List<OpenXRFeature>()
+        {
+            oculusTouch
+        }.ToArray();
+
         XRGeneralSettings.AttemptInitializeXRSDKOnLoad();
         XRGeneralSettings.AttemptStartXRSDKOnBeforeSplashScreen();
-        
+
         managerSettings.InitializeLoaderSync();
-        
+
         List<XRDisplaySubsystem> displays = new();
-        
+
         SubsystemManager.GetInstances(displays);
-        
+
         var myDisplay = displays[0];
         myDisplay.Start();
     }
