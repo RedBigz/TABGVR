daee3a1ce8931c9b6ff88572acd2de7ace7bae23
RedBigz <signoff@redbigz.com>
2024-11-06 17:30:38 +1000

    Update Kinematics to slightly fix jittering + fixing code up a bit more

--- TABGVR/Patches/KinematicsPatch.cs
+++ TABGVR/Patches/KinematicsPatch.cs
@@ -1,3 +1,5 @@
+using Epic.OnlineServices;
+using Epic.OnlineServices.Presence;
 using HarmonyLib;
 using TABGVR.Player;
 using UltimateIK;
@@ -5,10 +7,40 @@ using UnityEngine;
 
 namespace TABGVR.Patches;
 
-[HarmonyPatch(typeof(Holding), nameof(Holding.LateUpdate))]
+[HarmonyPatch(typeof(Holding))]
 class KinematicsPatch
 {
-    static void Postfix(Holding __instance)
+    static void SetupConnection(Rigidbody joint)
+    {
+        joint.GetComponentInChildren<Collider>().enabled = false;
+
+        // disable gravity in arms
+        joint.useGravity = false;
+        joint.transform.parent.Find(joint.gameObject.name).GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    static void UpdateConnection(Rigidbody joint, GameObject controller)
+    {
+        var controllerRelativeToGameCamera = controller.transform.position - Controllers.Head.transform.position +
+                                             Camera.current.transform.position;
+
+        if (Vector3.Distance(joint.position, controllerRelativeToGameCamera) < 0.01f) return;
+
+        joint.MovePosition(joint.position + controllerRelativeToGameCamera -
+            joint.transform.GetChild(0).position);
+    }
+
+    [HarmonyPatch(nameof(Holding.Start))]
+    [HarmonyPostfix]
+    static void StartPostfix(Holding __instance)
+    {
+        SetupConnection(__instance.rightHand);
+        SetupConnection(__instance.leftHand);
+    }
+
+    [HarmonyPatch(nameof(Holding.Update))]
+    [HarmonyPostfix]
+    static void UpdatePostfix(Holding __instance)
     {
         if (!Controllers.LeftHand || !Controllers.RightHand || !Controllers.Head) return;
         if (__instance.player != global::Player.localPlayer) return;
@@ -16,17 +48,7 @@ class KinematicsPatch
         // Plugin.Logger.LogInfo(
         //     $"KP {__instance.player} / Head: {Controllers.Head.transform.position} / Left: {Controllers.LeftHand.transform.position} / Right: {Controllers.RightHand.transform.position}");
 
-        __instance.rightHand.GetComponentInChildren<Collider>().enabled = false;
-        __instance.leftHand.GetComponentInChildren<Collider>().enabled = false;
-
-        __instance.rightHand.MovePosition(__instance.rightHand.position + Controllers.RightHand.transform.position -
-                                          Controllers.Head.transform.position +
-                                          Camera.current.transform.position -
-                                          __instance.rightHand.transform.GetChild(0).position);
-
-        __instance.leftHand.MovePosition(__instance.leftHand.position + Controllers.LeftHand.transform.position -
-                                         Controllers.Head.transform.position +
-                                         Camera.current.transform.position -
-                                         __instance.leftHand.transform.GetChild(0).position);
+        UpdateConnection(__instance.rightHand, Controllers.RightHand);
+        UpdateConnection(__instance.leftHand, Controllers.LeftHand);
     }
 }
