330886c91aad1ccfcabedcebba159f6e18af6006
RedBigz <signoff@redbigz.com>
2024-11-12 21:25:03 +1000

    Add holstering, reloading, picking up items and jumping

--- TABGVR/Player/VRControls.cs
+++ TABGVR/Player/VRControls.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.XR;
 
@@ -8,8 +9,12 @@ public class VRControls : MonoBehaviour
     private WeaponHandler weaponHandler;
     private MovementHandler movementHandler;
     private InputHandler inputHandler;
+    private InteractionHandler interactionHandler;
     private global::Player player;
     private Transform rotationX;
+    private HaxInput haxInput;
+
+    [CanBeNull] private Pickup currentPickup;
 
     internal const float TriggerDeadZone = 0.7f;
 
@@ -18,10 +23,31 @@ public class VRControls : MonoBehaviour
         weaponHandler = GetComponent<WeaponHandler>();
         movementHandler = GetComponent<MovementHandler>();
         inputHandler = GetComponent<InputHandler>();
+        interactionHandler = GetComponent<InteractionHandler>();
         player = GetComponent<global::Player>();
         rotationX = gameObject.GetComponentInChildren<RotationTarget>().transform.parent;
 
+        haxInput = HaxInput.Instance;
+
+        interactionHandler.canPickUpAction = pickup => currentPickup = pickup;
+
         inputHandler.enabled = false;
+    }
+
+    private bool _rightTriggered;
+    private bool _leftTriggered;
+
+    private bool _aButtonPressed;
+    private bool _bButtonPressed;
+    private bool _xButtonPressed;
+    private bool _yButtonPressed;
+    
+    private void PickupInteract()
+    {
+        if (currentPickup && !currentPickup.hasBeenPickedUp && interactionHandler.pickupCor == null)
+            interactionHandler.StartCoroutine(interactionHandler.StartPickup(currentPickup,
+                currentPickup.weaponType == global::Pickup.WeaponType.Weapon &&
+                !player.m_inventory.HasSpaceForWeapon(currentPickup)));
     }
 
     private void Update()
@@ -36,17 +62,52 @@ public class VRControls : MonoBehaviour
 
         Controllers.RightHandXR.TryGetFeatureValue(CommonUsages.primaryButton, out var aButton);
         Controllers.LeftHandXR.TryGetFeatureValue(CommonUsages.primaryButton, out var xButton);
-        
+
         Controllers.RightHandXR.TryGetFeatureValue(CommonUsages.secondaryButton, out var bButton);
         Controllers.LeftHandXR.TryGetFeatureValue(CommonUsages.secondaryButton, out var yButton);
+        
+        Controllers.RightHandXR.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out var rightClick);
 
         if (rightTrigger > TriggerDeadZone)
         {
-            weaponHandler.HoldAttack(true, false);
+            if (!_rightTriggered)
+            {
+                if (weaponHandler.rightWeapon) weaponHandler.PressAttack(true, false);
+                else PickupInteract();
+            }
+            else if (weaponHandler.rightWeapon) weaponHandler.HoldAttack(true, false);
         }
-        
+
+        if (leftTrigger > TriggerDeadZone)
+        {
+            if (!_leftTriggered)
+            {
+                if (weaponHandler.leftWeapon) weaponHandler.PressAttack(false, false);
+                else PickupInteract();
+            }
+            else if (weaponHandler.leftWeapon) weaponHandler.HoldAttack(false, false);
+        }
+
+        _rightTriggered = rightTrigger > TriggerDeadZone;
+        _leftTriggered = leftTrigger > TriggerDeadZone;
+
+        if (rightClick)
+            weaponHandler.CurrentWeapon = Pickup.EquipSlots.None;
+
         var movementVector = new Vector3(leftJoystick.x, 0.0f, leftJoystick.y);
 
         inputHandler.inputMovementDirection = rotationX.rotation * movementVector;
+
+        if (aButton && !_aButtonPressed) movementHandler.Jump();
+        if (bButton && !_bButtonPressed)
+        {
+            weaponHandler.rightWeapon?.gun.ReloadGun();
+            weaponHandler.leftWeapon?.gun.ReloadGun();
+        }
+
+        _aButtonPressed = aButton;
+        _bButtonPressed = bButton;
+        _xButtonPressed = xButton;
+        _yButtonPressed = yButton;
     }
 }
