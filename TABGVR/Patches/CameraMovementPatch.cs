db077e73bf9882f87c82de30d8dcce840c87ce57
RedBigz <signoff@redbigz.com>
2024-11-17 19:04:17 +1000

    Fix vehicles rotating camera position when entered at a nonzero angle and exited out

---
+++ TABGVR/Patches/CameraMovementPatch.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace TABGVR.Patches;
+
+[HarmonyPatch(typeof(CameraMovement))]
+public class CameraMovementPatch
+{
+    [HarmonyPatch(nameof(CameraMovement.LateUpdate))]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> LateUpdate(IEnumerable<CodeInstruction> instructions)
+    {
+        foreach (var instruction in instructions.Select((value, i) => new { i, value }))
+        {
+            // instruction.value.opcode == OpCodes.Ldarg_0 &&
+            //     instructionsList[instruction.i + 1].operand is FieldInfo fieldInfo &&
+            //     fieldInfo == typeof(CameraMovement).GetField(nameof(CameraMovement.sitting))
+
+            if (instruction.i is >= 144 and <= 196)
+            {
+                instruction.value.opcode = OpCodes.Nop;
+                instruction.value.operand = null;
+            }
+            
+            yield return instruction.value;
+        }
+    }
+
+    [HarmonyPatch(nameof(CameraMovement.ResetVehicleCamera))]
+    [HarmonyPrefix]
+    public static bool ResetVehicleCameraPatch() => false;
+}
