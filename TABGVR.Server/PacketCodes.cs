a8379e7e96d96ea438d6ea343b767297dfe0f81d
RedBigz <signoff@redbigz.com>
2024-12-14 13:09:18 +1000

    Add PacketCodes.cs

---
+++ TABGVR.Server/PacketCodes.cs
@@ -0,0 +1,79 @@
+namespace TABGVR.Server;
+
+public enum PacketCodes : byte
+{
+    /*
+     * Interrogate - Forces client to identify their VR Status
+     *
+     * Server Request: Empty
+     * Client Response: Empty
+     */
+    Interrogate = 196,
+    /*
+     * Tells client to assign VR components to a Player ID
+     *
+     * Server Request: byte playerIndex
+     * Client Response: Empty
+     */
+    Assign,
+    /*
+     * Send/receive controller data
+     *
+     * Client Request:
+     *  double hmdPosX,
+     *  double hmdPosY,
+     *  double hmdPosZ,
+     *
+     *  double hmdRotX,
+     *  double hmdRotY,
+     *  double hmdRotZ,
+     *
+     *  double leftControllerPosX,
+     *  double leftControllerPosY,
+     *  double leftControllerPosZ,
+     *
+     *  double leftControllerRotX,
+     *  double leftControllerRotY,
+     *  double leftControllerRotZ,
+     *
+     *  double rightControllerPosX,
+     *  double rightControllerPosY,
+     *  double rightControllerPosZ,
+     *
+     *  double rightControllerRotX,
+     *  double rightControllerRotY,
+     *  double rightControllerRotZ,
+     * 
+     * Server Response: Empty
+     *
+     * Server Request:
+     *  byte playerIndex,
+     *
+     *  double hmdPosX,
+     *  double hmdPosY,
+     *  double hmdPosZ,
+     *
+     *  double hmdRotX,
+     *  double hmdRotY,
+     *  double hmdRotZ,
+     *
+     *  double leftControllerPosX,
+     *  double leftControllerPosY,
+     *  double leftControllerPosZ,
+     *
+     *  double leftControllerRotX,
+     *  double leftControllerRotY,
+     *  double leftControllerRotZ,
+     *
+     *  double rightControllerPosX,
+     *  double rightControllerPosY,
+     *  double rightControllerPosZ,
+     *
+     *  double rightControllerRotX,
+     *  double rightControllerRotY,
+     *  double rightControllerRotZ,
+     *  
+     * Client Response: Empty
+     */
+    ControllerMotion = 216,
+}
