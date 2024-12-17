c9f9d15c1106f6e2682faffc9edeb9d58e38dc3a
RedBigz <signoff@redbigz.com>
2024-12-17 11:47:20 +1000

    Update SwapWeaponPatch.cs to use CodeMatcher instead of arbitrary hardcoded values (also adds a preprocessor if to the HarmonyDebug attribute)

--- TABGVR/Patches/SwapWeaponPatch.cs
+++ TABGVR/Patches/SwapWeaponPatch.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using HarmonyLib;
+using TABGInput;
 using TABGVR.Player;
 
 namespace TABGVR.Patches;
@@ -9,34 +10,24 @@ namespace TABGVR.Patches;
 [HarmonyPatch(typeof(InteractionHandler), nameof(InteractionHandler.StartPickup), MethodType.Enumerator)]
 public static class SwapWeaponPatch
 {
-    
-    
     [HarmonyTranspiler]
+#if DEBUG
     [HarmonyDebug]
-    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-    {
-        foreach (var instruction in instructions.Select((value, i) => new { i, value }))
-        {
-            Plugin.Logger.LogInfo($"{instruction.i}: {instruction.value}");
-            switch (instruction.i)
-            {
-                case 34:
-                    instruction.value.opcode = OpCodes.Call;
-                    instruction.value.operand = typeof(VRControls).GetMethod(nameof(VRControls.GetSomethingTriggered));
-                    
-                    yield return Transpilers.EmitDelegate(() => Plugin.Logger.LogInfo(VRControls.GetSomethingTriggered()));
-                    
-                    break;
-                case 33:
-                case 35:
-                case 36:
-                    instruction.value.opcode = OpCodes.Nop;
-                    break;
-            }
-            
-            Plugin.Logger.LogInfo($"=> {instruction.value}");
-    
-            yield return instruction.value;
-        }
-    }
+#endif
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) =>
+        new CodeMatcher(instructions)
+            .MatchForward(false,
+                new CodeMatch(OpCodes.Ldloc_1),
+                new CodeMatch(OpCodes.Ldfld,
+                    AccessTools.Field(typeof(InteractionHandler), nameof(InteractionHandler.m_PlayerActions))),
+                new CodeMatch(OpCodes.Ldfld,
+                    AccessTools.Field(typeof(PlayerActions), nameof(PlayerActions.Interact))),
+                new CodeMatch(OpCodes.Ldfld,
+                    AccessTools.Field(typeof(PlayerActions.HaxAction), nameof(PlayerActions.HaxAction.IsPressed))))
+            .SetOpcodeAndAdvance(OpCodes.Nop)
+            .SetInstructionAndAdvance(new CodeInstruction(OpCodes.Call,
+                AccessTools.Method(typeof(VRControls), nameof(VRControls.GetSomethingTriggered))))
+            .SetOpcodeAndAdvance(OpCodes.Nop)
+            .SetOpcodeAndAdvance(OpCodes.Nop)
+            .InstructionEnumeration();
 }
